c382be475ac8379f89b0f9957da548f45db5937d
Berlyng Yena <berlyngyg@gmail.com>
2025-08-01 00:44:25 -0400

    Inicio del proyecto usuario CRUD en consola

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsuariosCRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            menuPrincipal();
+
+        }
+
+        public static void menuPrincipal()
+        {
+            string opcion;
+
+            do
+            {
+                Console.WriteLine("===== MENÚ PRINCIPAL =====");
+                Console.WriteLine("1. Crear usuario");
+                Console.WriteLine("2. Listar usuarios");
+                Console.WriteLine("3. Editar usuario");
+                Console.WriteLine("4. Eliminar usuario");
+                Console.WriteLine("0. Salir");
+                Console.Write("Selecciona una opción: ");
+
+                opcion = Console.ReadLine();
+                Console.WriteLine($"Elegiste: {opcion}\n");
+
+            } while (opcion != "0");
+        }
+
+    }
+}
