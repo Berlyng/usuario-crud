72d5b4ac15f30fa357448e24c39bc70330e5b83e
Berlyng Yena <berlyngyg@gmail.com>
2025-08-01 00:52:16 -0400

    Agregando Menu principal

--- Program.cs
+++ Program.cs
@@ -17,23 +17,25 @@ namespace UsuariosCRUD
 
         public static void menuPrincipal()
         {
-            string opcion;
+                string opcion;
 
-            do
-            {
-                Console.WriteLine("===== MENÚ PRINCIPAL =====");
-                Console.WriteLine("1. Crear usuario");
-                Console.WriteLine("2. Listar usuarios");
-                Console.WriteLine("3. Editar usuario");
-                Console.WriteLine("4. Eliminar usuario");
-                Console.WriteLine("0. Salir");
-                Console.Write("Selecciona una opción: ");
+                do
+                {
+                    Console.WriteLine("===== MENÚ PRINCIPAL =====");
+                    Console.WriteLine("1. Crear usuario");
+                    Console.WriteLine("2. Listar usuarios");
+                    Console.WriteLine("3. Editar usuario");
+                    Console.WriteLine("4. Eliminar usuario");
+                    Console.WriteLine("0. Salir");
+                    Console.Write("Selecciona una opción: ");
 
-                opcion = Console.ReadLine();
-                Console.WriteLine($"Elegiste: {opcion}\n");
+                    opcion = Console.ReadLine();
+                    Console.WriteLine($"Elegiste: {opcion}\n");
 
-            } while (opcion != "0");
+                } while (opcion != "0");
         }
-
     }
+
 }
+
+
